a3a55ed1dad5260e46e6917743ce0e91c744670e
Adhu Haam <adhuhamlayaal55@gmail.com>
2025-03-29 02:44:18 +0500

    send

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { registerRootComponent } from 'expo';
-import App from './App';
-
-registerRootComponent(App);
-import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './App.js';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
